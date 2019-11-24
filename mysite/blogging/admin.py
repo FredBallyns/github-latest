58b3f96701a88d2bf4d7a4c960e961888f7d4780
Fred Ballyns <FredBallyns@gmail.com>
2019-11-23 21:14:41 -0800

    HW 07 Select Categories from Post view

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    inlines = [CategoryInline, ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    exclude = ('posts',)
+
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
